b01332f3621e34703f7f8373e341d3400f4bcaa7
alvaro308 <ortegaalvaro058@gmail.com>
2025-02-05 20:31:48 -0500

    carga de datos

--- botones.php
+++ botones.php
@@ -4,7 +4,7 @@
     <head>
         <link rel="stylesheet" href="estilos.css">
         <?php
-        $mostrar2= false;
+        $mostrar2 = false;
         $valor1 = 0;
         $num1 = 0;
         $num2 = 0;
@@ -12,8 +12,8 @@
         $mostrar = false;
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             if ($_POST["formulario"] == "suma") {
-              $num1 = $_POST["num1"] ?? 0;
-              $num2 = $_POST["num2"] ?? 0;
+                $num1 = $_POST["num1"] ?? 0;
+                $num2 = $_POST["num2"] ?? 0;
                 $valor1 = $num2 + $num1;
                 $mostrar2 = true;
             } else if ($_POST["formulario"] == "texto") {
@@ -44,7 +44,7 @@
             <div class="panel panel-left">
                 <form method="post">
                     <h4>funcion saludar</h4>
-                        <INput type="hidden" name="formulario" value="texto"></INput>
+                    <INput type="hidden" name="formulario" value="texto"></INput>
                     <button type="submit">saludar</button>
                 </form>
                 <br>
@@ -62,24 +62,26 @@
                 <button class="verde" onclick="cambiarFondo('#b9ff00')">Verde</button>
             </div>
             <div class="panel panel-left">
-            <form method="post">
-                <h4>formulario de registro</h4>
-            <label for="nombre">Nombre</label>
-           <input type="text" name="nombre1" id="nombre1" placeholder="ingrece su nombre" required>
-           <label for="seggundo apellido">Segundo apellido</label>
-            <input type="text" name="nombre2" id="nombre2" placeholder="ingrece su segundo nombre" required>
-            <label for="primer apellido">Primer apellido</label>
-            <input type="text" name="apellido1" id="apellido1" placeholder="ingrece su apellido" required>
-            <label for="segundo nombre">Segundo nombre</label>
-            <input type="text" name="apellido2" id="apellido2" placeholder="ingrece su segundo apellido" required>
-            <label for="correo electronico">Correo electronico</label>
-            <input type="email" name="corro electronico" id="correo electronico" placeholder="ingrece su correo electronico" required>
-            <label for="numero telefonico">Numero telefonico</label>
-            <input type="number" name="tel" id="tel" placeholder="ingrece numero telefonico" required>
-     <br>
-     <br>
-     <button> registrar </button>
-            </form>
+                <form method="post">
+                    <h4>formulario de registro</h4>
+                    <label for="nombre">Nombre</label>
+                    <input type="text" name="nombre1" id="nombre1" placeholder="ingrece su nombre" required>
+                    <label for="segundo apellido">Segundo apellido</label>
+                    <input type="text" name="nombre2" id="nombre2" placeholder="ingrece su segundo nombre" required>
+                    <label for="primer apellido">Primer apellido</label>
+                    <input type="text" name="apellido1" id="apellido1" placeholder="ingrece su apellido" required>
+                    <label for="segundo nombre">Segundo nombre</label>
+                    <input type="text" name="apellido2" id="apellido2" placeholder="ingrece su segundo apellido"
+                        required>
+                    <label for="correo electronico">Correo electronico</label>
+                    <input type="email" name="corro electronico" id="correo electronico"
+                        placeholder="ingrece su correo electronico" required>
+                    <label for="numero telefonico">Numero telefonico</label>
+                    <input type="number" name="tel" id="tel" placeholder="ingrece numero telefonico" required>
+                    <br>
+                    <br>
+                    <button> registrar </button>
+                </form>
             </div>
             <div class="panel panel-left">
                 <h4>
@@ -97,7 +99,7 @@
                 <br>
                 <?php if ($mostrar2): ?>
                     <label class="label1">
-                        <?php echo $num1+$num2; ?>
+                        <?php echo $num1 + $num2; ?>
 
                     </label>
                 <?php endif; ?>
