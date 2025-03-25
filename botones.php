e517f84f559235dba21e3f1e65c7d13bd357600a
alvaro308 <ortegaalvaro058@gmail.com>
2025-03-24 20:42:24 -0400

    nueva actualizacion

--- botones.php
+++ botones.php
@@ -17,20 +17,20 @@
         }
         if (isset($_POST['limpiar'])) {
             unset($_SESSION['registros']);
-            header("location:".$_SERVER ['PHP_SELF']);
+            header("location:" . $_SERVER['PHP_SELF']);
         }
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             if ($_POST["formulario"] == "suma") {
                 $num1 = $_POST["num1"] ?? 0;
                 $num2 = $_POST["num2"] ?? 0;
                 $num3 = $_POST["num3"] ?? 0;
-                $valor1  = ($num2 + $num1 + $num3 ) ;
+                $valor1 = ($num2 + $num1 + $num3);
                 $mostrar2 = true;
-            }else if ($_POST["formulario"] == "texto") {
+            } else if ($_POST["formulario"] == "texto") {
                 $saludo = "hola mundo3";
                 $mostrar = true;
-            }else if ($_POST["formulario"] == "registro") {
-      
+            } else if ($_POST["formulario"] == "registro") {
+
                 $registro = [
                     'nombre1' => $_POST["nombre1"] ?? "",
                     'nombre2' => $_POST["nombre2"] ?? "",
@@ -40,7 +40,8 @@
                     'tel' => $_POST["tel"] ?? "",
                 ];
                 $_SESSION['registros'][] = $registro;
-            }else{}
+            } else {
+            }
         }
         ?>
     </head>
@@ -64,7 +65,7 @@
         <?php include 'header.php'; ?>
         <div class="container">
             <div class="panel panel-left-3">
-            <form method="post">
+                <form method="post">
                     <h4>Formulario de Registro</h4>
                     <label for="nombre">Nombre</label>
                     <input type="text" name="nombre1" id="nombre1" placeholder="ingrese su nombre" required>
@@ -78,6 +79,31 @@
                     <label for="correo electronico">Correo Electrónico</label>
                     <input type="email" name="correo_electronico" id="correo_electronico"
                         placeholder="ingrese su correo electrónico" required>
+                    <label for="color de ojos">Color de ojos</label>
+                    <br>
+                    <select name="color_de_ojos" id="color_de_ojos">
+                        <option value="seleccione">color de ojos</option>
+                        <option value="verde">verde</option>
+                        <option value="azul">azul</option>
+                        <option value="cafe">cafe</option>
+                        <option value="Negro">Negro</option>
+                        <option value="Ámbar">Ámbar</option>
+                        <option value="Avellana">Avellana</option>
+                        <option value="gris">gris</option>
+
+                    </select>
+                    <br>
+                    <label for="edad">edad</label>
+                    <br>
+                    <select name="edad" id="edad">
+                    <option value="seleccione">ingrece su edad</option>
+        <?php 
+        for($i=1;$i<=100;$i++){
+           echo "<option value=\$i\">$i</option>" ; 
+        } 
+        ?>
+                    </select>
+                    <br>
                     <label for="numero telefonico">Número Telefónico</label>
                     <input type="number" name="tel" id="tel" placeholder="ingrese número telefónico" required>
                     <br>
@@ -85,81 +111,83 @@
                     <input type="hidden" name="formulario" value="registro">
                     <div style="display: flex; gap: 40%">
 
-                    <button type="submit">Registrar</button>
+                        <button type="submit">Registrar</button>
                 </form>
-                <form method="post"><button type="submit"name="limpiar" id>Eliminar datos</button>
-            </form>
+                <form method="post"><button type="submit" name="limpiar" id>Eliminar datos</button>
+                </form>
             </div>
             <br>
-                <table class="table-estilo-2">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Nombre</th>
-                            <th>Segundo nombre</th>
-                            <th>Primer apellido</th>
-                            <th>Segundo apellido</th>
-                            <th>Correo electronico</th>
-                            <th>Numero telefonico</th>
-                        </tr>
-                    </thead>
-                    <?php $acumulador=1 ?>
-                    <?php foreach ($_SESSION['registros'] as $registro): ?>
-                        <tr>
-                            <td><?php echo $acumulador ++; ?></td>
+            <table class="table-estilo-2">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Segundo nombre</th>
+                        <th>Primer apellido</th>
+                        <th>Segundo apellido</th>
+                        <th>Correo electronico</th>
+                        <th>color de ojos</th>
+                        <th>edad</th>
+                        <th>numero telefonico</th>
+                    </tr>
+                </thead>
+                <?php $acumulador = 1 ?>
+                <?php foreach ($_SESSION['registros'] as $registro): ?>
+                    <tr>
+                        <td><?php echo $acumulador++; ?></td>
 
-                            <td><?php echo htmlspecialchars($registro['nombre1']); ?></td>
-                            <td><?php echo htmlspecialchars($registro['nombre2']); ?></td>
-                            <td><?php echo htmlspecialchars($registro['apellido1']); ?></td>
-                            <td><?php echo htmlspecialchars($registro['apellido2']); ?></td>
-                            <td><?php echo htmlspecialchars($registro['correo_electronico']); ?></td>
-                            <td><?php echo htmlspecialchars($registro['tel']); ?></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </table>
-            </div>
-            <div class="panel panel-right">
-                <h4>cambiar el fondo de pantalla</h4>
-                <button class="rojo" onclick="cambiarFondo('#ff3131')">Rojo</button>
-                <button class="azul" onclick="cambiarFondo('#00f3ff')">Azul</button>
-                <button class="verde" onclick="cambiarFondo('#b9ff00')">Verde</button>
-                <button class="blanco" onclick="cambiarFondo('#ffffff')">blanco</button>
-            </div>
-           
-            <div class="panel panel-left">
-                <h4>
-                   calculadora
-                </h4>
-    <div class="calculadora">
-    
-                  <input type="text" id="pantalla"readonly>
-                  <br>
-                  <br>
-                  <div class="botones">
-                  <button onclick="agregarValor('7')">7</button>
-                  <button onclick="agregarValor('8')">8</button>
-                  <button onclick="agregarValor('9')">9</button>
-                  <button onclick="agregarValor('/')">/</button>
-                  <button onclick="agregarValor('4')">4</button>
-                  <button onclick="agregarValor('5')">5</button>
-                  <button onclick="agregarValor('6')">6</button>
-                  <button onclick="agregarValor('*')">*</button>
-                  <button onclick="agregarValor('1')">1</button>
-                  <button onclick="agregarValor('2')">2</button>
-                  <button onclick="agregarValor('3')">3</button>
-                  <button onclick="agregarValor('-')">-</button>
-                  <button onclick="agregarValor('0')">0</button>
-                  <button onclick="limpiarPantalla()">C</button>
-                  <button onclick="calcular()">=</button>
-                  <button onclick="agregarValor('+')">+</button>     
-                  <button onclick="borrarCaracter('←')">←</button>           
-                </div>
+                        <td><?php echo htmlspecialchars($registro['nombre1']); ?></td>
+                        <td><?php echo htmlspecialchars($registro['nombre2']); ?></td>
+                        <td><?php echo htmlspecialchars($registro['apellido1']); ?></td>
+                        <td><?php echo htmlspecialchars($registro['apellido2']); ?></td>
+                        <td><?php echo htmlspecialchars($registro['correo_electronico']); ?></td>
+                        <td><?php echo htmlspecialchars($registro['tel']); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
         </div>
-    </div>
-    </div>
+        <div class="panel panel-right">
+            <h4>cambiar el fondo de pantalla</h4>
+            <button class="rojo" onclick="cambiarFondo('#ff3131')">Rojo</button>
+            <button class="azul" onclick="cambiarFondo('#00f3ff')">Azul</button>
+            <button class="verde" onclick="cambiarFondo('#b9ff00')">Verde</button>
+            <button class="blanco" onclick="cambiarFondo('#ffffff')">blanco</button>
+        </div>
+
+        <div class="panel panel-left">
+            <h4>
+                calculadora
+            </h4>
+            <div class="calculadora">
+
+                <input type="text" id="pantalla" readonly>
+                <br>
+                <br>
+                <div class="botones">
+                    <button onclick="agregarValor('7')">7</button>
+                    <button onclick="agregarValor('8')">8</button>
+                    <button onclick="agregarValor('9')">9</button>
+                    <button onclick="agregarValor('/')">/</button>
+                    <button onclick="agregarValor('4')">4</button>
+                    <button onclick="agregarValor('5')">5</button>
+                    <button onclick="agregarValor('6')">6</button>
+                    <button onclick="agregarValor('*')">*</button>
+                    <button onclick="agregarValor('1')">1</button>
+                    <button onclick="agregarValor('2')">2</button>
+                    <button onclick="agregarValor('3')">3</button>
+                    <button onclick="agregarValor('-')">-</button>
+                    <button onclick="agregarValor('0')">0</button>
+                    <button onclick="limpiarPantalla()">C</button>
+                    <button onclick="calcular()">=</button>
+                    <button onclick="agregarValor('+')">+</button>
+                    <button onclick="borrarCaracter('←')">←</button>
+                </div>
+            </div>
+        </div>
+        </div>
         <div class="container">
             <div class="panel panel-left">
-            <form method="post">
+                <form method="post">
                     <h4>funcion saludar</h4>
                     <INput type="hidden" name="formulario" value="texto"></INput>
                     <button type="submit">saludar</button>
@@ -174,7 +202,7 @@
                 <?php endif; ?>
             </div>
             <div class="panel panel-left">
-            <h4>
+                <h4>
                     suma de numeros
                 </h4>
                 <form method="post">
@@ -191,40 +219,40 @@
                 <br>
                 <?php if ($mostrar2): ?>
                     <label class="label1">
-                        <?php echo $valor1/2; ?>
+                        <?php echo $valor1 / 2; ?>
 
                     </label>
                 <?php endif; ?>
             </div>
             <br>
-            </div>
-            <div class="panel panel-left">
-            </div>
-            <br>
+        </div>
+        <div class="panel panel-left">
+        </div>
+        <br>
         </div>
         <?php include 'footer.php'; ?>
     </body>
 
     </html>
     <script>
-        function agregarValor(numero){
-            document.getElementById("pantalla").value+=numero ;
+        function agregarValor(numero) {
+            document.getElementById("pantalla").value += numero;
         }
-        function limpiarPantalla(){
-            document.getElementById("pantalla").value="" ;
+        function limpiarPantalla() {
+            document.getElementById("pantalla").value = "";
         }
-        function calcular(){
+        function calcular() {
             try {
-                document.getElementById("pantalla").value=eval(document.getElementById("pantalla").value)   
+                document.getElementById("pantalla").value = eval(document.getElementById("pantalla").value)
             } catch (error) {
-               alert("error en la expresion") 
+                alert("error en la expresion")
             }
-  
+
         }
 
-        function borrarCaracter(){
-        var pantalla=document.getElementById("pantalla"); 
-        pantalla.value=pantalla.value.slice(0,-1) ;  
+        function borrarCaracter() {
+            var pantalla = document.getElementById("pantalla");
+            pantalla.value = pantalla.value.slice(0, -1);
         }
-    
+
     </script>
--- contacto.php
+++ contacto.php
@@ -1,33 +1,39 @@
-
 <!DOCTYPE html>
 <html>
+
 <head>
-<link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="estilos.css">
 </head>
-    <title>contacto</title>
-    <body>
-<?php include 'menu.php';?>
-<?php include 'header.php';?>
-<div class="container"> 
- <div class="panel-contacto">
-        <h1>
-            Contactanos
-        </h1>
-        <p>
-            Email: <a href="mailto:ortegaalvaro058@gmail.com">
-            ortegaalvaro058@gmail.com
-            </a>
-        </p>
-        <p>
-            Telefono: <a href="tel:8135079428">
-           8135079428
-            </a>
-        </p>
-        <p>
-            Direccion: 7735ironbarck Dr
-        </p>
+<title>contacto</title>
+
+<body>
+    <?php include 'menu.php'; ?>
+    <?php include 'header.php'; ?>
+    <div class="container2">
+        <div class="panel-contacto">
+            <h1>
+                Contactanos
+            </h1>
+            <p>
+                Email: <a href="mailto:ortegaalvaro058@gmail.com">
+                    ortegaalvaro058@gmail.com
+                </a>
+            </p>
+            <p>
+                Telefono: <a href="tel:8135079428">
+                    8135079428
+                </a>
+            </p>
+            <p>
+                Direccion: 7735ironbarck Dr
+            </p>
         </div>
-</div>
-<?php include 'footer.php';?>
-    </body>
+
+        <div class="panel-center">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3501.468350665071!2d-83.24499874543893!3d22.588954020282685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88ccc9fd656a72e1%3A0x982fee54ef3ebc93!2sLos%20Palacios%2C%20Cuba!5e1!3m2!1ses-419!2sus!4v1742515617136!5m2!1ses-419!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+    <?php include 'footer.php'; ?>
+</body>
+
 </html>
